854398e0c3bb170ea4f903b0fd78301ffdd51029
JohnnyDamasceno <147009274+JohnnyDamasceno@users.noreply.github.com>
2024-04-16 19:06:49 -0300

    Add files via upload

---
+++ Aula2.py
@@ -0,0 +1,13 @@
+#print("Olá, mundo, finalmente quebrei a maldição")
+
+#um comentario
+
+print("ola bem vindo ao nosso app!")
+
+nome = input("Qual é o seu nome?")
+idade = input("Qual a sua idade?")
+hobbie = input("Qual é o seu hobbie?")
+
+print(f"Olá {nome}, você tem {idade} anos, seu hobbie é {hobbie}")
+      
+#print("")
